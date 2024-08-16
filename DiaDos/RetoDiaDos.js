f08728d56fc4eff95fe61b18ed53559e1e5c2b5a
YesidBarrios <yesidbarrios2002@gmail.com>
2024-08-16 14:42:07 -0500

    add RetoDiaDos

---
+++ DiaDos/RetoDiaDos.js
@@ -0,0 +1,7 @@
+let nombre = prompt("¿Cual es tu Nombre?");
+let edad = prompt("¿Cual es tu edad?");
+let lenguaje = prompt("¿Que lenguaje de programación estás estudiando?");
+let gusta = prompt("¿Te gusta el lenguaje de programación que estás estudiando?: si o no?");
+let mensaje = `Hola ${nombre}, tienes ${edad} años, estas estudiando el lenguaje de programacion: ${lenguaje} y ${gusta} te gusta lo que estudias`;
+
+alert(mensaje);
