f1ffca6eb8bff178b6899d905a846643244f1b54
YesidBarrios <yesidbarrios2002@gmail.com>
2024-08-19 14:21:53 -0500

    update

--- Dia 5/RetoDiaCinco.js
+++ Dia 5/RetoDiaCinco.js
@@ -4,13 +4,11 @@ let lacteos = [];
 let congelados = [];
 let dulces = [];
 let verduras = [];
-let pregunta = prompt(
-  "¿Deseas agregar un alimento a tu lista de compras? (si/no)"
-);
+let pregunta = prompt( "¿Deseas agregar un alimento a tu lista de compras? (si/no)").toLowerCase();
 
 while (pregunta === "si") {
   let alimento = prompt("¿Qué alimento deseas agregar a tu lista de compras?");
-  let categoria = prompt( "¿En qué categoría se encaja ese alimento? (1: frutas, 2: lacteos, 3: congelados, 4: dulces, 5: verduras)");
+  let categoria = prompt( "¿En qué categoría se encaja ese alimento? selleciona un  numero => (1: frutas, 2: lacteos, 3: congelados, 4: dulces, 5: verduras)");
   if (categoria === "1") {
     frutas.push(alimento);
   } else if (categoria === "2") {
@@ -25,7 +23,7 @@ while (pregunta === "si") {
     console.log("Categoría no válida. Por favor, intenta de nuevo.");
   }
 
-  pregunta = prompt("¿Deseas agregar otro alimento a tu lista de compras? (si/no)");
+  pregunta = prompt("¿Deseas agregar otro alimento a tu lista de compras? (si/no)").toLowerCase();
 }
 
 console.log("Lista de compras:");
